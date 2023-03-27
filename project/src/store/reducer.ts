0ae2d2367f40175abfba898b0185fcbb9361dac7
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-03-27 11:20:22 +0300

    6-2

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -3,20 +3,14 @@ import { cards } from 'components/mocks/mocks';
 import { CITIES, sortList } from '../constants';
 import { changeCity, changeCardsSort } from './action';
 
-// Объект начального состояния
 const defaultState = {
-  city: CITIES[0], // город (используется для отбора списка предложений в определённом городе)
-  cards: cards, // список предложений по аренде.
-  sortOption: sortList[0] // список элементов сортировки
+  city: CITIES[0],
+  cards: cards,
+  sortOption: sortList[0]
 };
 
-// Функцию-редьюсер. Она принимает в качестве параметров текущий state и действие (action).
-// Результатом выполнения редьюсера станет новое состояние. Обратите внимание, для именования функций-редьюсеров применяются существительные.
-
-//вся логика тут
-
 const reducer = createReducer(defaultState, (builder) => {
-  builder // addCase. Первым аргументом он ожидает функцию, с помощью которой создаётся действие.
+  builder
     .addCase(changeCity, (state, action) => {
       state.city = action.payload;
     })
