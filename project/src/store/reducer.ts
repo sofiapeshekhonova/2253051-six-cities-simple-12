bfd9b03fd828be4b5626eebc4899d55570d36d44
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-03-28 10:46:46 +0300

    6-1

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,18 +1,21 @@
 import {createReducer} from '@reduxjs/toolkit';
 import { cards } from 'components/mocks/mocks';
+import { Card } from 'types/offer';
 import { CITIES } from '../constants';
 import { changeCity } from './action';
 
-// Объект начального состояния
-const defaultState = {
-  city: CITIES[0], // город (используется для отбора списка предложений в определённом городе)
-  cards: cards // список предложений по аренде.
+type InitialState = {
+  city: string;
+  cards: Card[];
 };
 
-// Функцию-редьюсер. Она принимает в качестве параметров текущий state и действие (action).
-// Результатом выполнения редьюсера станет новое состояние. Обратите внимание, для именования функций-редьюсеров применяются существительные.
 
-//вся логика тут
+// Объект начального состояния
+const defaultState: InitialState = {
+  city: CITIES[0],
+  cards: cards
+};
+
 
 const reducer = createReducer(defaultState, (builder) => {
   builder // addCase. Первым аргументом он ожидает функцию, с помощью которой создаётся действие.
