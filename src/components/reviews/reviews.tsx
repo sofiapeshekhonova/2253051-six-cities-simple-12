bf2594f05a124148d1119412220fc36ad240a9e0
Sofia Peshekhonova <Peshehonova94@yandex.ru>
2023-07-11 12:20:29 +0300

    final

---
+++ src/components/reviews/reviews.tsx
@@ -0,0 +1,27 @@
+import { ReviewsType } from '../../types/reviews';
+import ReviewsForm from 'components/reviews-form/reviews-form';
+import ReviewItem from 'components/review-item/review-item';
+import { useAppSelector } from 'hooks';
+import { getAuthorizationStatus } from 'store/user/selectors';
+import { AuthorizationStatus } from '../../constants';
+
+type Props = {
+  reviews: ReviewsType[];
+  roomId: number;
+  roomCommentsLength: number;
+}
+
+function Reviews({ reviews, roomId, roomCommentsLength }: Props): JSX.Element {
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  return (
+    <section className="property__reviews reviews">
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{roomCommentsLength}</span></h2>
+      <ul className="reviews__list">
+        {reviews.map((review) => <ReviewItem review={review} key={review.id} />)}
+      </ul>
+      {authorizationStatus === AuthorizationStatus.Auth && <ReviewsForm cardId={roomId}/> }
+    </section>
+  );
+}
+
+export default Reviews;
---
+++ src/types/offer.tsx
@@ -0,0 +1,48 @@
+// import { roomTypeToLable } from "constants";
+// import { RoomType } from "constants";
+
+import { RoomType } from '../constants';
+
+export type Host = {
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+ }
+
+export type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
+export type CityType = {
+  location: Location;
+  name: string;
+};
+
+export type Room = {
+  apartment: string;
+  room: string;
+  house: string;
+  hotel: string;
+};
+
+export type Card = {
+  title: string;
+  previewImage: string;
+  isPremium: boolean;
+  price: number;
+  type: keyof typeof RoomType;
+  id: number;
+  maxAdults: number;
+  bedrooms: number;
+  rating: number;
+  description?: string;
+  goods: string[];
+  images: string[];
+  host: Host;
+  length?: number;
+  city: CityType;
+  location: Location;
+ }
